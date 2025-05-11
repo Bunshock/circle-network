0deebfc6505627fdd70890f254f03c02ce183a0e
Bunshock <leomanto98@gmail.com>
2025-05-11 14:51:47 -0300

    Spring Initializr template

---
+++ CircleNetwork/src/main/java/com/bunshock/CircleNetwork/CircleNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bunshock.CircleNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleNetworkApplication.class, args);
+	}
+
+}
---
+++ CircleNetwork/src/test/java/com/bunshock/CircleNetwork/CircleNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunshock.CircleNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
